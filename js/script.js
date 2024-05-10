d3dfacbe99b8cf8e27ca378bd230de262f7d8c9b
Indira Dwi Rahma <indiradwirahma12@gmail.com>
2024-05-10 20:14:12 +0700

    Sidebar

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const nav = document.querySelector('.nav ul');
+    const hamburger = document.querySelector('.menu-toggle');
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+
+
+// Nampilin Hamburger dan Pengaturan klik
+document.addEventListener('click', function (event) {
+    const nav = document.querySelector('.nav ul');
+    const hamburger = document.querySelector('.menu-toggle');
+    const isClickInsideNav = nav.contains(event.target);
+
+    if (!isClickInsideNav && !hamburger.contains(event.target)) {
+        hamburger.classList.remove('active');
+        nav.classList.remove('active');
+    }
+});
